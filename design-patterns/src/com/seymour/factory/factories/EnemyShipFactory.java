c6d49add2d8022c3e67a102ecebde6446b574578
Anton Denisenko <badbondmimimirusc@yandex.ru>
2019-02-20 09:39:38 +0300

    phh2

--- design-patterns/src/com/seymour/factory/factories/EnemyShipFactory.java
+++ design-patterns/src/com/seymour/factory/factories/EnemyShipFactory.java
@@ -5,7 +5,7 @@ import com.seymour.factory.entities.EnemyShip;
 import com.seymour.factory.entities.RocketEnemyShip;
 import com.seymour.factory.entities.UFOEnemyShip;
 
-public class EnemyShipFactory {
+	public class EnemyShipFactory {
 
 	public EnemyShip makeEnemyShip(String shipType) {
 
@@ -13,13 +13,15 @@ public class EnemyShipFactory {
 
 		case "U":
 			return new UFOEnemyShip();
+				
 		case "R":
 			return new RocketEnemyShip();
+				
 		case "B":
 			return new BigUFOEnemyShip();
+				
 		default:
 			return null;
 		}
-
 	}
 }
